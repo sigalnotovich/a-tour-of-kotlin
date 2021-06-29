4b7fa42864ccacf7820b7e1a4d9b69421c9de522
Domrev Igor <domrevigor@gmail.com>
2021-06-29 18:20:30 +0300

    preparing ETL workshop

--- src/main/kotlin/etl/main.kt
+++ src/main/kotlin/etl/main.kt
@@ -3,6 +3,41 @@ package etl
 import java.sql.DriverManager
 import java.sql.SQLException
 
+
+/*
+                    ETL EXERCISE
+
+
+PART 1:
+            GIVEN THE API ( description in examples.http )
+            - Fetch all products
+            for every product in the list:
+                - update the product. set 'tags' to contain the ids of all other products within the same category.
+                - set the avgPrice attribute with the avg price of all products in the category
+                - load products to a mysql db
+                    - start by creating the schema
+                    - then load products to mysql
+
+            use functional programing style
+            write unit tests
+            use mockk to mock the access to mysql when writing
+            try to TDD
+            use dependency injection
+
+
+
+PART 2:
+            IMPLEMENT A SPRING CRUD API OVER THE PRODUCTS STORED IN MYSQL
+
+            - provide a search api that will allow fetching all products within a specific category, specific material4
+            - provide an api to get a product by id,name,
+            - provide an api to delete a product
+            - provide an api to update a product
+
+*/
+
+
+
 fun main() {
     val host = System.getenv("DB_HOST")
     val user = System.getenv("DB_USER")
