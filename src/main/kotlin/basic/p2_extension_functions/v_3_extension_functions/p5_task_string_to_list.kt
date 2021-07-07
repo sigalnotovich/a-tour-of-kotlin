bfcd4c51dd79de51c38975d929ee638ceea6c444
Domrev Igor <domrevigor@gmail.com>
2021-07-07 07:38:55 +0300

    .

--- src/main/kotlin/basic/p2_extension_functions/v_3_extension_functions/p5_task_string_to_list.kt
+++ src/main/kotlin/basic/p2_extension_functions/v_3_extension_functions/p5_task_string_to_list.kt
@@ -3,3 +3,5 @@ package basic.p2_extension_functions.v_3_extension_functions
 //todo: write a function that will turn a string into a list with a token
 
 fun <E> String.toLongList(token: String) : List<E> = TODO()
+
+// TOD ABUSE THIS !!!
